d3dcf681bae6ca663b3f3cad7a6cd3aff2bab0a4
barak98 <barak98@gmail.com>
2021-09-28 11:25:53 +0300

    ip update

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const init = async () => {
 	dotenv.config();
 	const config = {
 		port: process.env.PORT || 8080,
-		host: process.env.HOST || '0.0.0.0'
+		host: process.env.HOST || '35.204.176.196'
 	};
 	const server = await createServer( config );
 	await server.start();

fa6e91701d9bd7b4285ba83d5072c1c638b9601e
barak98 <barak98@gmail.com>
2021-09-28 11:32:57 +0300

    update to check if app works on localhost

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const init = async () => {
 	dotenv.config();
 	const config = {
 		port: process.env.PORT || 8080,
-		host: process.env.HOST || '35.204.176.196'
+		host: process.env.HOST || 'localhost'
 	};
 	const server = await createServer( config );
 	await server.start();
